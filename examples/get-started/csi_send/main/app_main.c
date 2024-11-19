019bf6c0bc109c77de8fd1a78472d196f4a4530d
ningyuxing <nyx19@foxmail.com>
2024-11-19 19:06:05 +0800

    get-started example add support for ESP32-C5 ESP32-C6

--- examples/get-started/csi_send/main/app_main.c
+++ examples/get-started/csi_send/main/app_main.c
@@ -21,6 +21,12 @@
 
 
 #define CONFIG_LESS_INTERFERENCE_CHANNEL    11
+#define CONFIG_WIFI_BAND_MODE_FOR_ESP32C5   WIFI_BAND_MODE_2G_ONLY
+#define CONFIG_WIFI_2G_BANDWIDTHS           WIFI_BW_HT40
+#define CONFIG_WIFI_5G_BANDWIDTHS           WIFI_BW_HT40
+#define CONFIG_WIFI_2G_PROTOCOL             WIFI_PROTOCOL_11N
+#define CONFIG_WIFI_5G_PROTOCOL             WIFI_PROTOCOL_11N
+#define CONFIG_ESP_NOW_PHYMODE              WIFI_PHY_MODE_HT40
 #define CONFIG_SEND_FREQUENCY               100
 
 static const uint8_t CONFIG_CSI_SEND_MAC[] = {0x1a, 0x00, 0x00, 0x00, 0x00, 0x00};
@@ -36,14 +42,39 @@ static void wifi_init()
 
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-    ESP_ERROR_CHECK(esp_wifi_set_bandwidth(ESP_IF_WIFI_STA, WIFI_BW_HT40));
     ESP_ERROR_CHECK(esp_wifi_start());
+#if CONFIG_IDF_TARGET_ESP32C5
+    esp_wifi_set_band_mode(CONFIG_WIFI_BAND_MODE_FOR_ESP32C5);
+#endif  
 
-    ESP_ERROR_CHECK(esp_wifi_config_espnow_rate(ESP_IF_WIFI_STA, WIFI_PHY_RATE_MCS0_SGI));
+    wifi_protocols_t protocols = {
+        .ghz_2g = CONFIG_WIFI_2G_PROTOCOL,
+        .ghz_5g = CONFIG_WIFI_5G_PROTOCOL
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_protocols(ESP_IF_WIFI_STA, &protocols));
+
+    wifi_bandwidths_t bandwidth = {
+        .ghz_2g = CONFIG_WIFI_2G_BANDWIDTHS,
+        .ghz_5g = CONFIG_WIFI_5G_BANDWIDTHS
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_bandwidths(ESP_IF_WIFI_STA, &bandwidth));
     ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
-
     ESP_ERROR_CHECK(esp_wifi_set_channel(CONFIG_LESS_INTERFERENCE_CHANNEL, WIFI_SECOND_CHAN_BELOW));
     ESP_ERROR_CHECK(esp_wifi_set_mac(WIFI_IF_STA, CONFIG_CSI_SEND_MAC));
+}
+
+static void wifi_esp_now_init(esp_now_peer_info_t peer) 
+{
+    ESP_ERROR_CHECK(esp_now_init());
+    ESP_ERROR_CHECK(esp_now_set_pmk((uint8_t *)"pmk1234567890123"));
+    esp_now_rate_config_t rate_config = {
+        .phymode = CONFIG_ESP_NOW_PHYMODE, 
+        .rate = WIFI_PHY_RATE_MCS0_LGI,    
+        .ersu = false,                     
+        .dcm = false                       
+    };
+    ESP_ERROR_CHECK(esp_now_add_peer(&peer));
+    ESP_ERROR_CHECK(esp_now_set_peer_rate_config(peer.peer_addr,&rate_config));
 }
 
 void app_main()
@@ -68,15 +99,13 @@ void app_main()
      * @breif Initialize ESP-NOW
      *        ESP-NOW protocol see: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/network/esp_now.html
      */
-    ESP_ERROR_CHECK(esp_now_init());
-    ESP_ERROR_CHECK(esp_now_set_pmk((uint8_t *)"pmk1234567890123"));
-
     esp_now_peer_info_t peer = {
         .channel   = CONFIG_LESS_INTERFERENCE_CHANNEL,
         .ifidx     = WIFI_IF_STA,    
         .encrypt   = false,   
         .peer_addr = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
     };
+    wifi_esp_now_init(peer);
     ESP_ERROR_CHECK(esp_now_add_peer(&peer));
 
     ESP_LOGI(TAG, "================ CSI SEND ================");
